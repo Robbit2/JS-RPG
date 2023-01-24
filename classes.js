1f087468ba20164435be358be0c36ad3c8241324
Qrexx <smeelyoinions@gmail.com>
2023-01-24 06:39:59 -0500

    updated item tooltips css and class

--- classes.js
+++ classes.js
@@ -4,7 +4,7 @@ const rarities = {
 }
 
 class Item {
-  constructor({name, level, gilded = false, rarity, type, item, img, strength, damage, defense, reforge, stars, id, weight, dmgBuff}){
+  constructor({name, level, gilded = false, rarity, type, item, img, strength, damage, defense, reforge, stars, id, weight, dmgBuff, strBuff}){
     this.name = name;
     this.level = level;
     this.gilded = gilded;
@@ -20,6 +20,7 @@ class Item {
     this.id = id;
     this.weight = weight;
     this.dmgBuff = dmgBuff;
+    this.strBuff = strBuff;
   }
 
   getName(){
@@ -29,7 +30,7 @@ class Item {
 }
 
 const items = {
-  copper_coin : new Item({name:"Copper Coin", level:1, gilded:false, rarity:"Common", type:"item.mainhand", item:"Collectable", img:"./img/copper_coin.png", strength:0, damage:0, defense:0, reforge:"E", stars:"", id:"item:copper_coin",weight:0.1, dmgBuff:1})
+  copper_coin : new Item({name:"Copper Coin", level:1, gilded:false, rarity:"Common", type:"item.mainhand", item:"Collectable", img:"./img/copper_coin.png", strength:0, damage:0, defense:0, reforge:"E", stars:"", id:"item:copper_coin",weight:0.1, dmgBuff:1, strBuff:1})
 };
 
 const searchItemDB = (query) => {
